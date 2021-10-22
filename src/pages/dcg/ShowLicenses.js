cedf51de20f8a4cb4c3828180c022ef9c77891a9
Princewill <ijeomahprincewill@gmail.com>
2021-10-22 09:47:25 +0100

    refactored filter license code

--- src/pages/dcg/ShowLicenses.js
+++ src/pages/dcg/ShowLicenses.js
@@ -16,53 +16,37 @@ const ShowLicenses = (props) => {
 
     console.log(props.efn);
 
-    if(props.efn !== ""){
-        licenseList = props.licenses.map(item => (
-            item.EXCISE_NUMBER === props.efn &&
-            <tr key={item.IDLICENSE} >
-                <td>{item.IDFORM}</td>
-                <td>{item.IDLICENSE}</td>
-                <td>{item.FORM41 ? item.FORM41[0].CNAME : "..."}</td>
-                <td>{removeDateParts(item.ISSUEDATE)}</td>
-                <td>{removeDateParts(item.EXPIRYDATE)}</td>
-                <td>{item.APPROVEDBY}</td>
-                <td>{item.EXCISE_NUMBER}</td>
-                <td>
-                    {item.STATUS === 1 && <span className="text text-primary">Provisional License</span>}
-                    {item.STATUS === 2 && <span className="text text-success">License</span>}
-                </td>
-                <td>
-                    {item.STATUS === 1 && <Link className="btn btn-link text-primary" to={`pr-license/${item.IDFORM}`}><i className="fa fa-search"></i></Link>}
-                    {item.STATUS === 2 && <Link className="btn btn-link text-primary" to={`license/${item.IDFORM}`}><i className="fa fa-search"></i></Link>}
-                    
-                </td>
-            </tr>
-    
-            
-        ))
+    let newList;
+
+    if(props.efn === ""){
+
+        newList = props.licenses;
     }
     else{
-        licenseList = props.licenses.map(item => (
-            <tr key={item.IDLICENSE} >
-                <td>{item.IDFORM}</td>
-                <td>{item.IDLICENSE}</td>
-                <td>{item.FORM41 ? item.FORM41[0].CNAME : "..."}</td>
-                <td>{removeDateParts(item.ISSUEDATE)}</td>
-                <td>{removeDateParts(item.EXPIRYDATE)}</td>
-                <td>{item.APPROVEDBY}</td>
-                <td>{item.EXCISE_NUMBER}</td>
-                <td>
-                    {item.STATUS === 1 && <span className="text text-primary">Provisional License</span>}
-                    {item.STATUS === 2 && <span className="text text-success">License</span>}
-                </td>
-                <td>
-                    {item.STATUS === 1 && <Link className="btn btn-link text-primary" to={`pr-license/${item.IDFORM}`}><i className="fa fa-search"></i></Link>}
-                    {item.STATUS === 2 && <Link className="btn btn-link text-primary" to={`license/${item.IDFORM}`}><i className="fa fa-search"></i></Link>}
-                    
-                </td>
-            </tr>            
-        ))
+        
+        newList = props.licenses.filter(licns => licns.EXCISE_NUMBER === props.efn);
     }
+
+    licenseList = newList.map(item => (
+        <tr key={item.IDLICENSE} >
+            <td>{item.IDFORM}</td>
+            <td>{item.IDLICENSE}</td>
+            <td>{item.FORM41 ? item.FORM41[0].CNAME : "..."}</td>
+            <td>{removeDateParts(item.ISSUEDATE)}</td>
+            <td>{removeDateParts(item.EXPIRYDATE)}</td>
+            <td>{item.APPROVEDBY}</td>
+            <td>{item.EXCISE_NUMBER}</td>
+            <td>
+                {item.STATUS === 1 && <span className="text text-primary">Provisional License</span>}
+                {item.STATUS === 2 && <span className="text text-success">License</span>}
+            </td>
+            <td>
+                {item.STATUS === 1 && <Link className="btn btn-link text-primary" to={`pr-license/${item.IDFORM}`}><i className="fa fa-search"></i></Link>}
+                {item.STATUS === 2 && <Link className="btn btn-link text-primary" to={`license/${item.IDFORM}`}><i className="fa fa-search"></i></Link>}
+                
+            </td>
+        </tr>        
+    ))
     
 
     return licenseList;
